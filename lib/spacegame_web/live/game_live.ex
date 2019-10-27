336bd3f9d2f2a8a96c1ac9896fe0311de2986d73
sorax <robert@hausgedacht.de>
2019-10-27 21:25:52 +0100

    add live-view

---
+++ lib/spacegame_web/live/game_live.ex
@@ -0,0 +1,18 @@
+defmodule SpacegameWeb.GameLive do
+  use Phoenix.LiveView
+
+  alias SpacegameWeb.PageView
+
+  def mount(_session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    # ~L"Rendering LiveView"
+    PageView.render("ship.html", assigns)
+  end
+
+  # def handle_event("test", _value, socket) do
+  #   {:noreply, socket}
+  # end
+end
