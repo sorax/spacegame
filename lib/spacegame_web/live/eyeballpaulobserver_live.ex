49750d3f9ffb0c35c51de242d49cc4458106c04c
sorax <robert@hausgedacht.de>
2019-12-17 23:00:48 +0100

    add paddle computation

--- lib/spacegame_web/live/eyeballpaulobserver_live.ex
+++ lib/spacegame_web/live/eyeballpaulobserver_live.ex
@@ -8,10 +8,10 @@ defmodule SpacegameWeb.EyeBallPaulObserverLive do
       socket
       |> assign(%{
         ball: %{x: 0, y: 0},
-        paddle: %{
-          deg: 0,
-          d: describe_arc(0, 0, 100, 0, 180)
-        }
+        paddles: [
+          get_paddle(90, 20, 1),
+          get_paddle(270, 20, 2)
+        ]
       })
 
     {:ok, socket}
@@ -23,67 +23,41 @@ defmodule SpacegameWeb.EyeBallPaulObserverLive do
     PageView.render("eyeballpaulobserver.html", assigns)
   end
 
-  defp polar_to_cartesian(center_x, center_y, radius, angle_in_degrees) do
-    angle_in_radians = (angle_in_degrees - 90) * :math.pi() / 180
-
+  def get_paddle(deg, width, player) do
     %{
-      x: center_x + radius * :math.cos(angle_in_degrees),
-      y: center_y + radius * :math.sin(angle_in_radians)
+      d: describe_arc(0, 0, 50, deg - width / 2, deg + width / 2),
+      color: if(player == 1, do: "255,66,0", else: "0,176,111")
     }
   end
 
-  defp describe_arc(x, y, radius, start_angle, end_angle) do
-    start_point = polar_to_cartesian(x, y, radius, end_angle)
-    end_point = polar_to_cartesian(x, y, radius, start_angle)
+  def describe_arc(x, y, radius, start_angle, end_angle) do
+    start_value = polar_to_cartesian(x, y, radius, end_angle)
+    end_value = polar_to_cartesian(x, y, radius, start_angle)
 
-    # large_arc_flag = endAngle - startAngle <= 180 ? "0" : "1"
-
-    large_arc_flag =
-      cond do
-        end_angle - start_angle == 180 -> "0"
-        end_angle - start_angle < 180 -> "0"
-        true -> "1"
-      end
+    large_arc_flag = if end_angle - start_angle <= 180, do: 0, else: 1
 
     [
       "M",
-      start_point.x,
-      start_point.y,
+      start_value.x,
+      start_value.y,
       "A",
       radius,
       radius,
       0,
       large_arc_flag,
       0,
-      end_point.x,
-      end_point.y
+      end_value.x,
+      end_value.y
     ]
     |> Enum.join(" ")
   end
+
+  def polar_to_cartesian(center_x, center_y, radius, angle_in_degrees) do
+    angle_in_radians = (angle_in_degrees - 90) * :math.pi() / 180.0
+
+    %{
+      x: round(center_x + radius * :math.cos(angle_in_radians)),
+      y: round(center_y + radius * :math.sin(angle_in_radians))
+    }
+  end
 end
-
-# function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-#   var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-
-#   return {
-#     x: centerX + (radius * Math.cos(angleInRadians)),
-#     y: centerY + (radius * Math.sin(angleInRadians))
-#   };
-# }
-
-# function describeArc(x, y, radius, startAngle, endAngle){
-
-#     var start = polarToCartesian(x, y, radius, endAngle);
-#     var end = polarToCartesian(x, y, radius, startAngle);
-
-#     var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-
-#     var d = [
-#         "M", start.x, start.y,
-#         "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-#     ].join(" ");
-
-#     return d;
-# }
-
-# document.getElementById("arc1").setAttribute("d", describeArc(200, 400, 100, 0, 180))
