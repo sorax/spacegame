fd3f2abf8a21ee2c075d0ccbd9b340e8a1a3d989
sorax <robert@hausgedacht.de>
2020-04-19 15:16:38 +0200

    set version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spacegame.MixProject do
   def project do
     [
       app: :spacegame,
-      version: "0.1.2",
+      version: "0.1.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
