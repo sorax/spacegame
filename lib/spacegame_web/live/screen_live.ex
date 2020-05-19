3a100ba2dae7c0e96c246c9962d982b8d760d39a
sorax <info@sorax.net>
2020-05-19 22:58:50 +0200

    show qr-code on screen (for controllers)

--- lib/spacegame_web/live/screen_live.ex
+++ lib/spacegame_web/live/screen_live.ex
@@ -3,6 +3,11 @@ defmodule SpacegameWeb.ScreenLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, ball: %{x: 0, y: 0}, paddles: [])}
+    qr =
+      (SpacegameWeb.Endpoint.url() <> Routes.controller_path(socket, :index))
+      |> EQRCode.encode()
+      |> EQRCode.svg()
+
+    {:ok, assign(socket, ball: %{x: 0, y: 0}, paddles: [], qr: qr)}
   end
 end
